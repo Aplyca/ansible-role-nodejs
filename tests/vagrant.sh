52c3bb550ad3b2ea8a3e557bd14b8dea5f365473
Mauricio Sánchez <msanchez@aplyca.com>
2016-09-09 17:54:13 -0500

    Fixing tests

--- tests/vagrant.sh
+++ tests/vagrant.sh
@@ -2,8 +2,5 @@
 # Test NodeJS provisiones with Ansible
 
 echo "Starting VM and provision with role"
-vagrant destroy -f
+vagrant destroy -f &&
 vagrant up
-
-echo "Running tests"
-ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory tests/playbooks.yml -u vagrant
